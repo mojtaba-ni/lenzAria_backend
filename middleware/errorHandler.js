06ebcc7a853054f431ba48749ab6a3a1aa3807fe
mojtaba_ni <mojtabanik@yahoo.com>
2024-04-20 23:07:25 +0330

    create errorHandler

---
+++ middleware/errorHandler.js
@@ -0,0 +1,40 @@
+import { constans } from "../public/constans.js";
+
+const errorHandler = (err, req, res, next) => {
+  const statusCode = res.statuseCode ? res.statusCode : 500;
+  switch (statusCode) {
+    case constans.NOT_FOUND:
+      res.json({
+        title: "Not Found",
+        message: err.message,
+        stackTrace: err.stack,
+      });
+      break;
+    case constans.UNAUTHORIZED:
+      res.json({
+        title: "Unauthorized",
+        message: err.message,
+        stackTrace: err.stack,
+      });
+      break;
+    case constans.FORBIDDEN:
+      res.json({
+        title: "Forbidden",
+        message: err.message,
+        stackTrace: err.stack,
+      });
+      break;
+    case constans.SERVER_ERROR:
+      res.json({
+        title: "Server Error",
+        message: err.message,
+        stackTrace: err.stack,
+      });
+      break;
+    default:
+        console.log("No Error, All good");
+      break;
+  }
+};
+
+export default errorHandler;
