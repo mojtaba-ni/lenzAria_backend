54c8fe9b42616e24347718376465b1b051c2fc04
mojtaba_ni <mojtabanik@yahoo.com>
2025-05-26 14:45:54 +0330

    feat: add updateBrand functionality and refactor existing brand operations

--- controller/brandController.js
+++ controller/brandController.js
@@ -3,51 +3,76 @@ import errorHandler from "../middleware/errorHandler.js";
 import { SuccesResponse } from "../config/response.js";
 import brandModel from "../model/brandModel.js";
 
-
 //@ desc getAllBrand
 //@ route POST api/brand/add
 //@ access public
 export const getAllBrand = expressAsyncHandler(async (req, res) => {
-    const blogs = await brandModel.find();
-    res.status(200).json(SuccesResponse(blogs));
-})
+  const blogs = await brandModel.find();
+  res.status(200).json(SuccesResponse(blogs));
+});
 
 //@ desc addBrand
 //@ route POST api/brand/add
 //@ access public
 export const addBrand = expressAsyncHandler(async (req, res) => {
-    const { name} = req.body;
-    if (!name) {
-      res.status(400);
-      throw new errorHandler("Please fill out all");
-    }
-    const checkBrand = await brandModel.findOne({name})
-    if(checkBrand){
-        res.status(400);
-        throw new errorHandler("its already exist");
-    }
-    await brandModel.create({
-      name
-    });
-    res.status(200).json(SuccesResponse());
+  const { name } = req.body;
+  if (!name) {
+    res.status(400);
+    throw new errorHandler("Please fill out all");
+  }
+  const checkBrand = await brandModel.findOne({ name });
+  if (checkBrand) {
+    res.status(400);
+    throw new errorHandler("its already exist");
+  }
+  await brandModel.create({
+    name,
+  });
+  res.status(200).json(SuccesResponse());
 });
 
 //@ desc deleteBlog
 //@ route DELETE api/blog/delete
 //@ access public
 export const deleteBrand = expressAsyncHandler(async (req, res) => {
-    const { brandId } = req.body;
-    const findBrand = await brandModel.findOne({ _id: brandId });
-    if (!brandId) {
-      res.status(400);
-      throw new errorHandler("Please fill out blogId");
-    }
-    if (!findBrand) {
-      res.status(404);
-      throw new errorHandler("blog not exist or its already removed");
-    }
-    await brandModel.deleteOne({ _id: brandId });
-    res.status(200).send(SuccesResponse());
-    return;
-  });
-  
+  const { brandId } = req.body;
+  const findBrand = await brandModel.findOne({ _id: brandId });
+  if (!brandId) {
+    res.status(400);
+    throw new errorHandler("Please fill out blogId");
+  }
+  if (!findBrand) {
+    res.status(404);
+    throw new errorHandler("blog not exist or its already removed");
+  }
+  await brandModel.deleteOne({ _id: brandId });
+  res.status(200).send(SuccesResponse());
+  return;
+});
+
+//@ desc updateBrand
+//@ route PUT api/brand/update
+//@ access public
+export const updateBrand = expressAsyncHandler(async (req, res) => {
+  const { brandId, name } = req.body;
+  if (!brandId || !name) {
+    res.status(400);
+    throw new errorHandler("Please fill out all");
+  }
+  const findBrand = await brandModel.findOne({ _id: brandId });
+  if (!findBrand) {
+    res.status(404);
+    throw new errorHandler("برندی یافت نشد یا حذف شده است");
+  }
+  const checkBrand = await brandModel.findOne({ name });
+  if (checkBrand) {
+    res
+      .status(200)
+      .json({
+        message: "برندی  با این اسم قبلا ایجاد شده است",
+        isSuccess: false,
+      });
+  }
+  await brandModel.updateOne({ _id: brandId }, { name });
+  res.status(200).json(SuccesResponse());
+});
