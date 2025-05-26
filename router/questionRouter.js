ed4831a3a4f3ea4aeaab08e8226d3d12ce12f83f
mojtaba_ni <mojtabanik@yahoo.com>
2025-05-26 14:46:44 +0330

    feat: add editQuestion route and update questionRouter with new functionality

--- router/questionRouter.js
+++ router/questionRouter.js
@@ -1,9 +1,14 @@
 import express from "express";
-import { addQuestion, deleteQuestion, getAllQuestion } from "../controller/questionController.js";
-
+import {
+  addQuestion,
+  deleteQuestion,
+  editQuestion,
+  getAllQuestion,
+} from "../controller/questionController.js";
 
 export const quetionRoutes = express.Router();
 
 quetionRoutes.post("/add", addQuestion);
 quetionRoutes.get("/getAllQuestion", getAllQuestion);
-quetionRoutes.delete("/delete" , deleteQuestion)
+quetionRoutes.delete("/delete", deleteQuestion);
+quetionRoutes.put("/edit", editQuestion);
